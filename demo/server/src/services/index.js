d4ad464d21337530f001aa974c71f2b1db339941
Kaufmann, Roman <roman.kaufmann.at@gmail.com>
2016-12-10 23:47:01 +0100

    Fixed this errors

---
+++ demo/server/src/services/index.js
@@ -0,0 +1,13 @@
+'use strict';
+const notes = require('./notes');
+const authentication = require('./authentication');
+const user = require('./user');
+
+module.exports = function() {
+  const app = this;
+
+
+  app.configure(authentication);
+  app.configure(user);
+  app.configure(notes);
+};
